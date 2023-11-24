aa89e21c926b89bdcb9fd0a6758f1b7333caaf31
Alan <alan.ionita@perceptual-robotics.com>
2023-11-24 10:32:06 +0000

    FEAT (cy) : adds custom command to get elements by data-test attribute

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,16 +1,22 @@
-describe('Fundamentals test', () => {
-  beforeEach(()=> {
-    cy.visit('/fundamentals')
-  })
-  it('Contains correct header text', () => {
-    cy.get('[data-test="fundamentals-header"]').contains(/Testing Fundamentals/i);
-  })
-  it('Accordion performs correctly', () => {
+describe("Fundamentals test", () => {
+  beforeEach(() => {
+    cy.visit("/fundamentals");
+  });
+  it("Contains correct header text", () => {
+    cy.getDataTest("fundamentals-header").contains(/Testing Fundamentals/i);
+  });
+  it("Accordion performs correctly", () => {
     // cy.get('[data-test="accordion-1"]').contains(/Describe blocks/i);
-    cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
+    cy.contains(/Your tests will exist in a describe block/i).should(
+      "not.be.visible"
+    );
     cy.get('[data-test="accordion-1"] div[role="button"]').click();
-    cy.contains(/Your tests will exist in a describe block/i).should('be.visible')
+    cy.contains(/Your tests will exist in a describe block/i).should(
+      "be.visible"
+    );
     cy.get('[data-test="accordion-1"] div[role="button"]').click();
-    cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
-  })
-})
+    cy.contains(/Your tests will exist in a describe block/i).should(
+      "not.be.visible"
+    );
+  });
+});
