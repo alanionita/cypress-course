55e1bcbfb5b3d5714a962170d23b4fb884f36833
Alan <alan.ionita@perceptual-robotics.com>
2023-11-24 10:28:17 +0000

    REFACTOR (cy) : implemented beforeEach to navigate to the page

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,10 +1,11 @@
 describe('Fundamentals test', () => {
-  it('Contains correct header text', () => {
+  beforeEach(()=> {
     cy.visit('/fundamentals')
+  })
+  it('Contains correct header text', () => {
     cy.get('[data-test="fundamentals-header"]').contains(/Testing Fundamentals/i);
   })
   it('Accordion performs correctly', () => {
-    cy.visit('/fundamentals')
     // cy.get('[data-test="accordion-1"]').contains(/Describe blocks/i);
     cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
     cy.get('[data-test="accordion-1"] div[role="button"]').click();
