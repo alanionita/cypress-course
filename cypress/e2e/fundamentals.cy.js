668ac691ff96c3b38a7d09683ab54ed38962caa8
Alan <alan.ionita@perceptual-robotics.com>
2023-11-24 10:25:48 +0000

    FEAT (cy): adds Accordion data attribute and test for correct behaviour
    Test opening and closing the accordion

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -3,4 +3,13 @@ describe('Fundamentals test', () => {
     cy.visit('/fundamentals')
     cy.get('[data-test="fundamentals-header"]').contains(/Testing Fundamentals/i);
   })
+  it('Accordion performs correctly', () => {
+    cy.visit('/fundamentals')
+    // cy.get('[data-test="accordion-1"]').contains(/Describe blocks/i);
+    cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
+    cy.get('[data-test="accordion-1"] div[role="button"]').click();
+    cy.contains(/Your tests will exist in a describe block/i).should('be.visible')
+    cy.get('[data-test="accordion-1"] div[role="button"]').click();
+    cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
+  })
 })
